f64088a11ac2c0eb8d24f49fc894ba90ce2d648d
Nathan Tipper <n.tipper@uleth.ca>
2019-02-27 01:42:32 -0700

    A triangle has been born unto the world!

--- src/main.cc
+++ src/main.cc
@@ -3,16 +3,17 @@
 #include <GLFW/glfw3.h>
 #include <string>
 #include <fstream>
+#include <cstring>
 
 typedef unsigned short ushort;
 typedef unsigned int uint;
 
 void framebuffer_size_callback(GLFWwindow*, int, int);
 void process_input(GLFWwindow*);
-const GLchar* const get_shader_source(const std::string);
+const char* get_shader_source(const std::string&);
 
-const std::string VERTEX_SHADER_SOURCE = "../shaders/shader.vert"; 
-const std::string FRAGMENT_SHADER_SOURCE = "../shaders/shader.frag";
+const std::string VERTEX_SHADER_SOURCE = "./shaders/shader.vert"; 
+const std::string FRAGMENT_SHADER_SOURCE = "./shaders/shader.frag";
 
 int main() {
   // Initialize GLFW
@@ -44,9 +45,14 @@ int main() {
 		       0.0f, 0.5f, 0.0f
   };
 
+  // Create a VAO which will store the VBO's data
+  uint VAO;
+  glGenVertexArrays(1, &VAO);
+ 
   // Generate a buffer with the array of vertices
   uint VBO;
   glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -68,7 +74,7 @@ int main() {
   }
 
 
-  const GLchar* const fragmentShaderSource = get_shader_source(FRAGMENT_SHADER_SOURCE);
+  const char* fragmentShaderSource = get_shader_source(FRAGMENT_SHADER_SOURCE);
   // Create and compile the fragment shader
   uint fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -81,6 +87,34 @@ int main() {
     std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
+  // Create the Shader program
+  uint shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  // Test to see if the Shader program has compiled
+  if(!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER PROGRAM::COMPILATION FAILED\n" << infoLog << std::endl;
+  }
+  
+  // Use the program
+  glUseProgram(shaderProgram);
+
+  // Delete shaders as we no longer need them
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // We must tell OpenGL how to process the vertex data
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
   
   // render
   while(!glfwWindowShouldClose(window)) {
@@ -91,11 +125,19 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // draw triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
   glfwTerminate();
   
   return 0;
@@ -110,21 +152,26 @@ void process_input(GLFWwindow* window) {
     glfwSetWindowShouldClose(window, true);
 }
 
-const GLchar* const get_shader_source(std::string path) {
-  std::ifstream is(path);
+const char* get_shader_source(const std::string& path) {
+  std::ifstream is;
+  is.open(path);
+
   std::string source("");
   if(is.is_open()) {
     char c;
     while(is.get(c)) {
       source += c;
     }
-
+    
     is.close();
   }
   else {
     std::cout << "Was not able to open file " << path << std::endl;
   }
 
-  std::cout << source << std::endl;
-  return source.c_str();
+
+  char* output = new char[source.length()];
+  strcpy(output, source.c_str());
+  
+  return output;
 }
