4635488e98c7644ce4563f4d4227ce398d139c04
Nathan Tipper <n.tipper@uleth.ca>
2019-02-27 00:08:38 -0700

    added shaders and modified Makefile to add a bin file.

--- Makefile
+++ Makefile
@@ -7,21 +7,22 @@ CCFLAGS = -g
 SRC_DIR = ./src
 INC_DIR = ./include
 OBJ_DIR = ./obj
+EXE_DIR = ./bin
 
 OBJECTS = $(addprefix $(OBJ_DIR)/, main.o)
 LDLIBS = -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lXxf86vm -lXinerama \
 -lXcursor -lrt -lm
 
 $(OBJ_DIR)/%.o : $(SRC_DIR)/%.cc
-	@echo compiling c++ files
+	@echo "\n******** Compiling C++ files ********\n"
 	$(CXX) -I$(INC_DIR) -c $(CXXFLAGS) $^ -o $@
 
-engine: $(OBJECTS) glad.o
-	@echo Starting installation
-	$(CXX) $(CXXFLAGS) -o engine $(OBJECTS) $(OBJ_DIR)/glad.o $(LDLIBS) 
+engine: $(OBJECTS) glad.o | $(EXE_DIR)
+	@echo "\n******** Linking ********\n"
+	$(CXX) $(CXXFLAGS) -o $(EXE_DIR)/engine $(OBJECTS) $(OBJ_DIR)/glad.o $(LDLIBS) 
 
 glad.o : $(SRC_DIR)/glad.c
-	@echo Compiling glad
+	@echo "\n******** Compiling glad ********\n"
 	$(CC) -I$(INC_DIR) -c $(CCFLAGS) $^ -o $(OBJ_DIR)/$@
 
 $(OBJECTS) : | $(OBJ_DIR)
@@ -29,9 +30,12 @@ $(OBJECTS) : | $(OBJ_DIR)
 $(OBJ_DIR):
 	mkdir $(OBJ_DIR)
 
+$(EXE_DIR):
+	mkdir $(EXE_DIR)
+
 .PHONY : clean cleanall
 clean:
-	rm -fr obj *~ engine
+	rm -fr obj bin *~
 
 cleanall: clean
 	rm -fr src/*~ include/*~
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <string>
+#include <fstream>
 
-typedef unsigned short ushort; 
+typedef unsigned short ushort;
+typedef unsigned int uint;
 
 void framebuffer_size_callback(GLFWwindow*, int, int);
+void process_input(GLFWwindow*);
+const GLchar* const get_shader_source(const std::string);
+
+const std::string VERTEX_SHADER_SOURCE = "../shaders/shader.vert"; 
+const std::string FRAGMENT_SHADER_SOURCE = "../shaders/shader.frag";
 
 int main() {
   // Initialize GLFW
@@ -31,7 +39,59 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  float vertices[] = { -0.5f, -0.5f, 0.0f,
+		       0.5f, -0.5f, 0.0f,
+		       0.0f, 0.5f, 0.0f
+  };
+
+  // Generate a buffer with the array of vertices
+  uint VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  const GLchar* const vertexShaderSource = get_shader_source(VERTEX_SHADER_SOURCE);
+  // Create and compile the vertex shader
+  uint vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Check for shader compilation success
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if(!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
+
+  const GLchar* const fragmentShaderSource = get_shader_source(FRAGMENT_SHADER_SOURCE);
+  // Create and compile the fragment shader
+  uint fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if(!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
+  
+  // render
   while(!glfwWindowShouldClose(window)) {
+    // input
+    process_input(window);
+
+    // rendering commands here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -43,4 +103,28 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+const GLchar* const get_shader_source(std::string path) {
+  std::ifstream is(path);
+  std::string source("");
+  if(is.is_open()) {
+    char c;
+    while(is.get(c)) {
+      source += c;
+    }
+
+    is.close();
+  }
+  else {
+    std::cout << "Was not able to open file " << path << std::endl;
+  }
+
+  std::cout << source << std::endl;
+  return source.c_str();
 }
